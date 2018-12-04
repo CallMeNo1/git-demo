a3d8fad4ad524889ab8e41da9666214997d2d955
CallMeNo1 <1542327427@qq.com>
2018-12-04 16:56:52 +0800

    图灵api测试

---
+++ tuling_api.py
@@ -0,0 +1,15 @@
+# I was changed in pop branch
+
+import json
+import requests
+apikey = 'f575876d96df4419bf8e1dcfc6e94777'  # 贝康apikey'
+
+def get_response(msg):
+    key = apikey
+    api = 'http://www.tuling123.com/openapi/api?key={}&info={}'.format( key, msg)
+    return requests.get(api).json()
+
+while True:
+    text= input('User：')
+    message = get_response(text)
+    print(message)
